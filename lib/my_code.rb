f081b2cc24ba3f7dff21dda47e2f029a95303be4
Phillip Henry III <philtrader71@gmail.com>
2020-01-03 23:23:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,59 @@
 # My Code here....
+def map_to_negativize(array)
+new = []
+  i = 0
+  while i < array.length do
+    new << ( array[i] * -1 )
+    i += 1
+  end
+  return new
+end
+
+def map_to_no_change(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new << ( array[i] )
+    i += 1
+  end
+  return new
+end
+
+def map_to_double(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new << ( array[i] * 2 )
+    i += 1
+  end
+  return new
+end
+
+def map_to_square(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new << ( array[i] * array[i] )
+    i += 1
+  end
+  return new
+end
+
+def reduce_to_total(array, starting_point=0)
+  new = starting_point
+  i = 0
+  while i < array.length do
+    new += array[i]
+    i += 1
+  end
+  return new
+end
+
+def reduce_to_all_true(array)
+  i = 0
+  while i < source_array.length do
+    return false if !array[i]
+    i += 1
+  end
+  return true
+end
